c833a74db2fee5e15d8ab7aa14408e26ff0ee009
Younes AIT SI ABBOU <younesaitsiabou@gmail.com>
2025-12-22 12:58:11 +0100

    adding html

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "strongCyan": "hsl(171, 66%, 44%)",
+        "lightBlue": "hsl(233, 100%, 69%)",
+        "darkGrayishBlue": "hsl(210, 10%, 33%)",
+        "grayishBlue": "hsl(201, 11%, 66%)"
+      },
+      fontFamily:{
+        "Bai Jamjuree": []
+      }
+    },
+  },
+  plugins: [],
+}
